5bf8ce88eba77f1ccf878476ed8e1d9ad0c54104
Bernardo Rocha Salgueiro <bernardo.roll@gmail.com>
2016-12-21 13:52:41 -0200

    Removed some comments

--- app/src/main/java/com/bernardo/myappportfolio/presenter/DashboardPresenter.java
+++ app/src/main/java/com/bernardo/myappportfolio/presenter/DashboardPresenter.java
@@ -1,19 +1,5 @@
 package com.bernardo.myappportfolio.presenter;
 
 public interface DashboardPresenter extends CommonPresenter {
-
-    String getPopularMoviesString();
-    String getStockHawkString();
-    String getBuildItBiggerString();
-    String getMakeYourAppMaterialString();
-    String getGoUbiquitousString();
-    String getCapstoneString();
-
-    String getPopularMoviesLaunchAppString();
-    String getStockHawkLaunchAppString();
-    String getBuildItBiggerLaunchAppString();
-    String getMakeYourAppMaterialLaunchAppString();
-    String getGoUbiquitousLaunchAppString();
-    String getCapstoneLaunchAppString();
-
+    String getTextResource(int resourceId);
 }
--- app/src/main/java/com/bernardo/myappportfolio/viewmodel/DashboardViewModel.java
+++ app/src/main/java/com/bernardo/myappportfolio/viewmodel/DashboardViewModel.java
@@ -1,5 +1,6 @@
 package com.bernardo.myappportfolio.viewmodel;
 
+import com.bernardo.myappportfolio.R;
 import com.bernardo.myappportfolio.presenter.DashboardPresenter;
 
 public class DashboardViewModel {
@@ -12,51 +13,52 @@ public class DashboardViewModel {
     }
 
     public void didClickPopularMoviesButton() {
-        this.presenter.showToast(this.presenter.getPopularMoviesLaunchAppString());
+        this.presenter.showToast(this.presenter.getTextResource(R.string.popular_movies_lauch_app));
     }
 
     public void didClickStockHawkButton() {
-        this.presenter.showToast(this.presenter.getStockHawkLaunchAppString());
+        this.presenter.showToast(this.presenter.getTextResource(R.string.stock_hawk_launch_app));
     }
 
     public void didClickBuildItBiggerButton() {
-        this.presenter.showToast(this.presenter.getBuildItBiggerLaunchAppString());
+        this.presenter.showToast(this.presenter.getTextResource(R.string.build_it_bigger_launch_app));
     }
 
     public void didClickMakeYourAppMaterialButton() {
-        this.presenter.showToast(this.presenter.getMakeYourAppMaterialLaunchAppString());
+        this.presenter.showToast(this.presenter
+                .getTextResource(R.string.make_your_app_material_launch_app));
     }
 
     public void didClickGoUbiquitousButton() {
-        this.presenter.showToast(this.presenter.getGoUbiquitousLaunchAppString());
+        this.presenter.showToast(this.presenter.getTextResource(R.string.go_ubiquitous_launch_app));
     }
 
     public void didClickCapstoneButton() {
-        this.presenter.showToast(this.presenter.getCapstoneLaunchAppString());
+        this.presenter.showToast(this.presenter.getTextResource(R.string.capstone_launch_app));
     }
 
     public String getPopularMoviesName() {
-        return this.presenter.getPopularMoviesString();
+        return this.presenter.getTextResource(R.string.popular_movies);
     }
 
     public String getStockHawkName() {
-        return this.presenter.getStockHawkString();
+        return this.presenter.getTextResource(R.string.stock_hawk);
     }
 
     public String getBuildItBiggerName() {
-        return this.presenter.getBuildItBiggerString();
+        return this.presenter.getTextResource(R.string.build_it_bigger);
     }
 
     public String getMakeYourAppMaterialName() {
-        return this.presenter.getMakeYourAppMaterialString();
+        return this.presenter.getTextResource(R.string.make_your_app_material);
     }
 
     public String getGoUbiquitousName() {
-        return this.presenter.getGoUbiquitousString();
+        return this.presenter.getTextResource(R.string.go_ubiquitous);
     }
 
     public String getCapstoneName() {
-        return this.presenter.getCapstoneString();
+        return this.presenter.getTextResource(R.string.capstone);
     }
 
 }
